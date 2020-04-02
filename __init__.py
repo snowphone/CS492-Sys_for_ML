51ac91ee376be66278c73c73ef0f009508334c17
Junoh Moon <mjo970625@gmail.com>
2020-04-02 15:24:21 +0000

    Fill out blanked codes
    I didn't test yet, but I wrote most of logical flows that the homework requires.
    I wrote draw function and main logic. However, I didn't understand 4th requirements.

--- __init__.py
+++ __init__.py
@@ -1,84 +1,111 @@
 import sys
 import numpy as np
-import cv2 as cv2
-import time
+import cv2
+import datetime
 import yolov2tiny
 
-def open_video_with_opencv(in_video_path, out_video_path):
-    #
-    # This function takes input and output video path and open them.
-    #
-    # Your code from here. You may clear the comments.
-    #
-    print('open_video_with_opencv is not yet implemented')
-    sys.exit()
 
-    # Open an object of input video using cv2.VideoCapture.
+def open_video_with_opencv(
+        in_video_path: str,
+        out_video_path: str) -> (cv2.VideoCapture, cv2.VideoWriter):
 
+	reader = cv2.VideoCapture(in_video_path)
+	if not reader.isOpened():
+		raise Exception("Failed to open '{}'".format(in_video_path))
 
-    # Open an object of output video using cv2.VideoWriter.
+	writer = cv2.VideoWriter(out_video_path)
+	if not writer.isOpened():
+		raise Exception(
+		    "Failed to create video named '{}'".format(out_video_path))
 
-
-    # Return the video objects and anything you want for further process.
-
-
-def resize_input(im):
-    imsz = cv2.resize(im, (416, 416))
-    imsz = imsz / 255.
-    imsz = imsz[:,:,::-1]
-    return np.asarray(imsz, dtype=np.float32)
-
-def video_object_detection(in_video_path, out_video_path, proc="cpu"):
-    #
-    # This function runs the inference for each frame and creates the output video.
-    #
-    # Your code from here. You may clear the comments.
-    #
-    print('video_object_detection is not yet implemented')
-    sys.exit()
-
-    # Open video using open_video_with_opencv.
+	return reader, writer
 
 
-    # Check if video is opened. Otherwise, exit.
+def resize_input(im: np.ndarray) -> np.ndarray:
+	imsz = im.resize(im, (416, 416))
+	imsz = imsz / 255.
+	imsz = imsz[:, :, ::-1]
+	return np.asarray(imsz, dtype=np.float32)
 
 
-    # Create an instance of the YOLO_V2_TINY class. Pass the dimension of
-    # the input, a path to weight file, and which device you will use as arguments.
+def draw(image: np.ndarray, proposals: list) -> np.ndarray:
+	'''
+	Draw bounding boxes into image and return it
+
+	proposals contains a list of (best_class_name, lefttop, rightbottom, color).
+	'''
+	for name, lefttop, rightbottom, color in proposals:
+		height, width, _channel = image.shape
+		lefttop = int(lefttop[0] * width), int(lefttop[1] * height)
+		rightbottom = int(lefttop[0] * width), int(lefttop[1] * height)
+
+		image = cv2.rectangle(image, lefttop, rightbottom, color, 3)
+		cv2.putText(image, name, (lefttop[0], lefttop[1] - 10),
+		            cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, color)
+
+	return image
 
 
-    # Start the main loop. For each frame of the video, the loop must do the followings:
-    # 1. Do the inference.
-    # 2. Run postprocessing using the inference result, accumulate them through the video writer object.
-    #    The coordinates from postprocessing are calculated according to resized input; you must adjust
-    #    them to fit into the original video.
-    # 3. Measure the end-to-end time and the time spent only for inferencing.
-    # 4. Save the intermediate values for the first layer.
-    # Note that your input must be adjusted to fit into the algorithm,
-    # including resizing the frame and changing the dimension.
+def video_object_detection(in_video_path: str,
+                           out_video_path: str,
+                           proc="cpu"):
+	"""
+	Read a videofile, scan each frame and draw objects using pretrained yolo_v2_tiny model.
+	Finally, store drawed frames into `out_video_path`
+	"""
+	reader, writer = open_video_with_opencv(in_video_path, out_video_path)
+	yolo = yolov2tiny.YOLO_V2_TINY((416, 416, 3), "./y2t_weights.pickle", proc)
 
+	acc_time, acc_cnt = 0, 0
+	while reader.isOpened():
+		okay, image = reader.read()
+		if not okay:
+			raise Exception(
+			    "An error occured while reading from {}".format(in_video_path))
 
-    # Check the inference peformance; end-to-end elapsed time and inferencing time.
-    # Check how many frames are processed per second respectivly.
-    
+		image = resize_input(image)
+		beg = datetime.datetime.now()
+		batched_tensors = yolo.inference(image)
+		end = datetime.datetime.now()
+		inference_time = (end - beg).total_seconds()
 
-    # Release the opened videos.
-    
+		acc_time += inference_time
+		acc_cnt += 1
+
+		print(
+		    "#i: done in {} seconds\ttotal:{} seconds\tthroughput: {} frames per second"
+		    .format(acc_cnt, inference_time, acc_time, acc_cnt / acc_time))
+
+		for tensor in batched_tensors:
+			proposals = yolov2tiny.postprocessing(tensor)
+			out_image = draw(image, proposals)
+			writer.write(out_image)
+
+	reader.release()
+	writer.release()
+
+	return
+
+	# 4. Save the intermediate values for the first layer.
+
 
 def main():
-    if len(sys.argv) < 3:
-        print ("Usage: python3 __init__.py [in_video.mp4] [out_video.mp4] ([cpu|gpu])")
-        sys.exit()
+	if len(sys.argv) < 3:
+		print(
+		    "Usage: python3 __init__.py [in_video.mp4] [out_video.mp4] ([cpu|gpu])"
+		)
+		sys.exit()
 
-    in_video_path = sys.argv[1] 
-    out_video_path = sys.argv[2] 
+	in_video_path = sys.argv[1]
+	out_video_path = sys.argv[2]
 
-    if len(sys.argv) == 4:
-        proc = sys.argv[3]
-    else:
-        proc = "cpu"
+	if len(sys.argv) == 4:
+		proc = sys.argv[3]
+	else:
+		proc = "cpu"
 
-    video_object_detection(in_video_path, out_video_path, proc)
+	video_object_detection(in_video_path, out_video_path, proc)
+
 
 if __name__ == "__main__":
-    main()
+	main()
