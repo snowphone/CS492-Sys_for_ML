a6a6bf5b0b416826bdeb75a65ff03f92b80157a1
Junoh Moon <mjo970625@gmail.com>
2020-05-20 21:31:17 +0900

    Set indents to tab

--- proj3/src/yolov2tiny.py
+++ proj3/src/yolov2tiny.py
@@ -121,38 +121,38 @@ def postprocessing(predictions):
   
     # IMPORTANT: Compute the coordinates and score of the B-Boxes by considering the parametrization of YOLOv2
     for row in range(n_grid_cells):
-      for col in range(n_grid_cells):
-        for b in range(n_b_boxes):
+        for col in range(n_grid_cells):
+            for b in range(n_b_boxes):
   
-          tx, ty, tw, th, tc = predictions[row, col, b, :5]
+                tx, ty, tw, th, tc = predictions[row, col, b, :5]
   
-          # IMPORTANT: (416 img size) / (13 grid cells) = 32!
-          # YOLOv2 predicts parametrized coordinates that must be converted to full size
-          # final_coordinates = parametrized_coordinates * 32.0 ( You can see other EQUIVALENT ways to do this...)
-          center_x = (float(col) + sigmoid(tx)) * 32.0
-          center_y = (float(row) + sigmoid(ty)) * 32.0
+                # IMPORTANT: (416 img size) / (13 grid cells) = 32!
+                # YOLOv2 predicts parametrized coordinates that must be converted to full size
+                # final_coordinates = parametrized_coordinates * 32.0 ( You can see other EQUIVALENT ways to do this...)
+                center_x = (float(col) + sigmoid(tx)) * 32.0
+                center_y = (float(row) + sigmoid(ty)) * 32.0
   
-          roi_w = np.exp(tw) * anchors[2*b + 0] * 32.0
-          roi_h = np.exp(th) * anchors[2*b + 1] * 32.0
+                roi_w = np.exp(tw) * anchors[2*b + 0] * 32.0
+                roi_h = np.exp(th) * anchors[2*b + 1] * 32.0
   
-          final_confidence = sigmoid(tc)
+                final_confidence = sigmoid(tc)
   
-          # Find best class
-          class_predictions = predictions[row, col, b, 5:]
-          class_predictions = softmax(class_predictions)
+                # Find best class
+                class_predictions = predictions[row, col, b, 5:]
+                class_predictions = softmax(class_predictions)
   
-          class_predictions = tuple(class_predictions)
-          best_class = class_predictions.index(max(class_predictions))
-          best_class_score = class_predictions[best_class]
+                class_predictions = tuple(class_predictions)
+                best_class = class_predictions.index(max(class_predictions))
+                best_class_score = class_predictions[best_class]
    
-          # Flip the coordinates on both axes
-          left   = int(center_x - (roi_w/2.))
-          right  = int(center_x + (roi_w/2.))
-          top    = int(center_y - (roi_h/2.))
-          bottom = int(center_y + (roi_h/2.))
-          
-          if( (final_confidence * best_class_score) > 0.3):
-            thresholded_predictions.append([[left, top, right, bottom], final_confidence * best_class_score, classes[best_class]])
+                # Flip the coordinates on both axes
+                left   = int(center_x - (roi_w/2.))
+                right  = int(center_x + (roi_w/2.))
+                top    = int(center_y - (roi_h/2.))
+                bottom = int(center_y + (roi_h/2.))
+                
+                if( (final_confidence * best_class_score) > 0.3):
+                    thresholded_predictions.append([[left, top, right, bottom], final_confidence * best_class_score, classes[best_class]])
   
     # Sort the B-boxes by their final score
     thresholded_predictions.sort(key=lambda tup: tup[1], reverse=True)
@@ -177,54 +177,54 @@ def postprocessing(predictions):
     return label_boxes
 
 def iou(boxA,boxB):
-  # boxA = boxB = [x1,y1,x2,y2]
-
-  # Determine the coordinates of the intersection rectangle
-  xA = max(boxA[0], boxB[0])
-  yA = max(boxA[1], boxB[1])
-  xB = min(boxA[2], boxB[2])
-  yB = min(boxA[3], boxB[3])
-
-  # Compute the area of intersection
-  intersection_area = (xB - xA + 1) * (yB - yA + 1)
-
-  # Compute the area of both rectangles
-  boxA_area = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
-  boxB_area = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
-
-  # Compute the IOU
-  iou = intersection_area / float(boxA_area + boxB_area - intersection_area)
-
-  return iou
+    # boxA = boxB = [x1,y1,x2,y2]
+  
+    # Determine the coordinates of the intersection rectangle
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+  
+    # Compute the area of intersection
+    intersection_area = (xB - xA + 1) * (yB - yA + 1)
+  
+    # Compute the area of both rectangles
+    boxA_area = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxB_area = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+  
+    # Compute the IOU
+    iou = intersection_area / float(boxA_area + boxB_area - intersection_area)
+  
+    return iou
 
 def non_maximal_suppression(thresholded_predictions, iou_threshold):
-
-  nms_predictions = []
-
-  # Add the best B-Box because it will never be deleted
-  nms_predictions.append(thresholded_predictions[0])
-
-  # For each B-Box (starting from the 2nd) check its iou with the higher score B-Boxes
-  # thresholded_predictions[i][0] = [x1,y1,x2,y2]
-  i = 1
-  while i < len(thresholded_predictions):
-    n_boxes_to_check = len(nms_predictions)
-    #print('N boxes to check = {}'.format(n_boxes_to_check))
-    to_delete = False
-
-    j = 0
-    while j < n_boxes_to_check:
-        curr_iou = iou(thresholded_predictions[i][0],nms_predictions[j][0])
-        if(curr_iou > iou_threshold ):
-            to_delete = True
-        #print('Checking box {} vs {}: IOU = {} , To delete = {}'.format(thresholded_predictions[i][0],nms_predictions[j][0],curr_iou,to_delete))
-        j = j+1
-
-    if to_delete == False:
-        nms_predictions.append(thresholded_predictions[i])
-    i = i+1
-
-  return nms_predictions
+  
+    nms_predictions = []
+  
+    # Add the best B-Box because it will never be deleted
+    nms_predictions.append(thresholded_predictions[0])
+  
+    # For each B-Box (starting from the 2nd) check its iou with the higher score B-Boxes
+    # thresholded_predictions[i][0] = [x1,y1,x2,y2]
+    i = 1
+    while i < len(thresholded_predictions):
+        n_boxes_to_check = len(nms_predictions)
+        #print('N boxes to check = {}'.format(n_boxes_to_check))
+        to_delete = False
+  
+        j = 0
+        while j < n_boxes_to_check:
+            curr_iou = iou(thresholded_predictions[i][0],nms_predictions[j][0])
+            if(curr_iou > iou_threshold ):
+                to_delete = True
+            #print('Checking box {} vs {}: IOU = {} , To delete = {}'.format(thresholded_predictions[i][0],nms_predictions[j][0],curr_iou,to_delete))
+            j = j+1
+  
+        if to_delete == False:
+            nms_predictions.append(thresholded_predictions[i])
+        i = i+1
+  
+    return nms_predictions
 
 def sigmoid(x):
     return 1 / (1 + np.e ** -x)
